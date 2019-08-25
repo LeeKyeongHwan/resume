936957f15d8664c67da159643add11caa66b8f6a
LeeKyeongHwan <rudghks7816@gmail.com>
2019-08-25 16:45:19 +0900

    # dist 폴더 테스트

---
+++ dist/precache-manifest.53ab888d694ff75f52ea3a32bfba2c6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aaa81751810b0710f326",
+    "url": "/js/chunk-vendors.a0fd4fc0.js"
+  },
+  {
+    "revision": "870cff0f99764b8ffaca",
+    "url": "/js/app.15a28510.js"
+  },
+  {
+    "revision": "6684e8c248e4ac3f8184",
+    "url": "/js/about.9d250a95.js"
+  },
+  {
+    "revision": "bc00a6db64ff065ee36a2935adfe2b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "870cff0f99764b8ffaca",
+    "url": "/css/app.9c62b63d.css"
+  },
+  {
+    "revision": "6684e8c248e4ac3f8184",
+    "url": "/css/about.5ccd5796.css"
+  }
+];
